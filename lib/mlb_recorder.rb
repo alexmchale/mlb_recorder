a341b946cbcfc93218ba917a036eef034b56d890
Alex McHale <alex@anticlever.com>
2014-07-23 07:25:34 -0400

    Add a message when a gem can't be loaded

--- lib/mlb_recorder.rb
+++ lib/mlb_recorder.rb
@@ -1,19 +1,28 @@
-require "time"
-require "date"
-require "net/http"
-require "json"
-require "ap"
-require "shellwords"
+begin
 
-require "thor"
-require "terminal-table"
-require "chronic"
-require "active_support/time"
+  require "time"
+  require "date"
+  require "net/http"
+  require "json"
+  require "shellwords"
 
-require "mlb_recorder/extensions"
-require "mlb_recorder/mlb_recorder"
-require "mlb_recorder/mlb_recording_monitor"
-require "mlb_recorder/mlb_game_list"
-require "mlb_recorder/mlb_game"
-require "mlb_recorder/mlb_team"
-require "mlb_recorder/record_app"
+  require "ap"
+  require "thor"
+  require "terminal-table"
+  require "chronic"
+  require "active_support/time"
+
+  require "mlb_recorder/extensions"
+  require "mlb_recorder/mlb_recorder"
+  require "mlb_recorder/mlb_recording_monitor"
+  require "mlb_recorder/mlb_game_list"
+  require "mlb_recorder/mlb_game"
+  require "mlb_recorder/mlb_team"
+  require "mlb_recorder/record_app"
+
+rescue LoadError
+
+  puts "gem install --no-rdoc --no-ri awesome_print thor terminal-table chronic activesupport"
+  exit
+
+end
