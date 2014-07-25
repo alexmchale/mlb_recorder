bba6a15bbc1517eb733690d5d1796f4d984477f2
Alex McHale <alex@anticlever.com>
2014-07-25 08:39:52 -0500

    Use bundler

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'awesome_print', require: 'ap'
+gem 'thor'
+gem 'terminal-table'
+gem 'chronic'
+gem 'activesupport', require: 'active_support/time'
+gem 'term-ansicolor', require: 'term/ansicolor'
+gem 'typhoeus'
--- lib/mlb_recorder.rb
+++ lib/mlb_recorder.rb
@@ -1,30 +1,20 @@
-begin
+# Load standard libraries
+require "time"
+require "date"
+require "net/http"
+require "json"
+require "shellwords"
+require "rubygems"
 
-  require "time"
-  require "date"
-  require "net/http"
-  require "json"
-  require "shellwords"
+# Load rubygems
+require "bundler/setup"
+Bundler.require(:default)
 
-  require "ap"
-  require "thor"
-  require "terminal-table"
-  require "chronic"
-  require "active_support/time"
-  require "term/ansicolor"
-  require "typhoeus"
-
-  require "mlb_recorder/extensions"
-  require "mlb_recorder/mlb_recorder"
-  require "mlb_recorder/mlb_recording_monitor"
-  require "mlb_recorder/mlb_game_list"
-  require "mlb_recorder/mlb_game"
-  require "mlb_recorder/mlb_team"
-  require "mlb_recorder/record_app"
-
-rescue LoadError
-
-  puts "gem install --no-rdoc --no-ri awesome_print thor terminal-table chronic activesupport term-ansicolor typhoeus"
-  exit
-
-end
+# Load local libraries
+require "mlb_recorder/extensions"
+require "mlb_recorder/mlb_recorder"
+require "mlb_recorder/mlb_recording_monitor"
+require "mlb_recorder/mlb_game_list"
+require "mlb_recorder/mlb_game"
+require "mlb_recorder/mlb_team"
+require "mlb_recorder/record_app"
